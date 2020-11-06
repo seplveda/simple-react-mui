0ef7b47b3f7faa86ded9f8d838be1784d86db28a
Rodrigo Sepúlveda <seplveda@gmail.com>
2020-11-06 12:55:09 -0300

    Added Tasks component && component dir

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 
 import tasks from './sample/task.json';
+import Tasks from './components/Tasks';
 
 
 class App extends Component {
@@ -14,6 +15,7 @@ class App extends Component {
     return <div>
       { this.state.tasks.map( e => <p key={e.id}>
         {e.title} - {e.description} - {e.done} - {e.id}
+        <Tasks/>
       </p>) }
     </div>
   }
